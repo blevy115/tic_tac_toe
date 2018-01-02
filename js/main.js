a074db3784e2f3bc335b65033888ea4a83b9cd99
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-01 23:39:05 -0600

    added alternating 'x' and 'o' functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,17 @@
 document.addEventListener('DOMContentLoaded', function () {
   var square = document.getElementsByClassName('square')
-
+  var counter = 0
   for (var i=0; square.length>i; i++){
     square[i].addEventListener('click', function(e){
-        this.style.backgroundColor = "red";
+        var img = document.createElement('img')
+        if (counter%2===1){
+        img.src = "img/0.png"
+      }else if (counter%2===0){
+        img.src = "img/x.png"
+      }
+      this.appendChild(img)
+      counter ++
+        // this.style.backgroundColor = "red";
     });
   };
 })

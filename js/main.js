f70a077eb1f8725dd7eaee479963a5ca631987b9
Brandon Levy <brandon.levy@mail.mcgill.ca>
2018-01-02 09:29:30 -0600

    added limiter of one entry per square

--- js/main.js
+++ js/main.js
@@ -3,15 +3,19 @@ document.addEventListener('DOMContentLoaded', function () {
   var counter = 0
   for (var i=0; square.length>i; i++){
     square[i].addEventListener('click', function(e){
+      if (this.classList.contains('o') === false &&
+          this.classList.contains('x') === false){
         var img = document.createElement('img')
         if (counter%2===1){
         img.src = "img/0.png"
+        this.className = 'square o'
       }else if (counter%2===0){
         img.src = "img/x.png"
+        this.className = 'square x'
       }
       this.appendChild(img)
       counter ++
-        // this.style.backgroundColor = "red";
+    };
     });
   };
 })
